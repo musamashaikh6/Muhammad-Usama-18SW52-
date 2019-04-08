cf159da2ede1e4549b0242bcecc387543fa0ef44
musamashaikh6 <49339592+musamashaikh6@users.noreply.github.com>
2019-04-09 00:41:09 +0500

    Add files via upload

---
+++ OOP-LAB1/Task1.java
@@ -0,0 +1,12 @@
+class Task1
+{
+public static void main(String[]args)
+{
+String name="Muhammad Usama";
+int marks= 880;
+float per = 80.0f;
+String grade="A1";
+
+System.out.println("Name="+name+"\n"+"Marks="+marks+"\n"+"Percentage="+per+"\n"+"Grade="+grade);
+}
+}
---
+++ OOP-LAB1/Task2.java
@@ -0,0 +1,11 @@
+class Task2
+{
+public static void main(String[]args)
+{
+double d=50.20;
+long a=(int)50.20;
+double b=(d-a);
+
+System.out.println("Number="+d+"\n"+"Integral Part="+a+"\n"+"Fractional Part="+b);
+}
+}
